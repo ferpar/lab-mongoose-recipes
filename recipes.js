20f3c7a0cfb1eec683e314e0146507148291ddb2
Fer Perez Ayala <ferp@protonmail.ch>
2018-11-14 23:29:33 +0100

    Iteration 2 complete.

--- recipes.js
+++ recipes.js
@@ -9,7 +9,7 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err);
   });
 
-const rescipeSchema = new Schema({
+const recipeSchema = new Schema({
   title: { type: String, required: true, unique: true },
   level: { type: String, enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'] },
   ingredients: { type: Array },
@@ -19,4 +19,20 @@ const rescipeSchema = new Schema({
   duration: {type: Number, min: 0},
   creator: {type: String},
   created: {type: Date, default: Date.now}
+});
+
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+
+Recipe.create({
+  title: 'Asian Glazed Chicken Thighs',
+  level: 'Amateur Chef',
+  ingredients: ['1/2 cup rice vinegar', '5 tablespoons honey', '1/3 cup soy sauce (such as Silver Swan®)', '1/4 cup Asian (toasted) sesame oil', '3 tablespoons Asian chili garlic sauce', '3 tablespoons minced garlic', 'salt to taste', '8 skinless, boneless chicken thighs'],
+  cuisine: 'Asian',
+  dishType: ['Dish'],
+  image: 'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+  duration: 40,
+  creator: 'Chef LePapu'
 })
+  .then(recipe => { console.log('title: ', recipe.title) })
+  .catch(err => { console.log('An error happened:', err) })
